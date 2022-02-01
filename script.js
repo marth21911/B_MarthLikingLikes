202380bdcb859e3612adaf988f4a341b17bc9bc3
Brian Marth <marthbrian@gmail.com>
2022-02-01 09:05:00 -0500

    likinglikesv1

---
+++ script.js
@@ -0,0 +1,18 @@
+var friends= document.querySelector("#friends");
+var value= 0
+function clickevent(){
+    value++;
+    friends.innerText= value+" Likes";
+}
+var friends2= document.querySelector("#friends2");
+var value2= 0
+function clickevent2(){
+    value2++;
+    friends2.innerText= value2+" Likes";
+}
+var friends3= document.querySelector("#friends3");
+var value3= 0
+function clickevent3(){
+    value3++;
+    friends3.innerText= value3+" Likes";
+}
